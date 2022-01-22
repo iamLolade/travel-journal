4e92fa90d2b6cb1c227c4f59aaf5d5c3e846336f
iamLolade <adeniyiotemade@gmail.com>
2022-01-22 14:10:52 +0100

    Created Navbar component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="">
+      <Navbar />
+      <h1>Welcome</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import globeLogo from "../images/Path.png"
+const Navbar = () => {
+    return ( 
+        <nav>
+            <img src={globeLogo} alt="app-logo" />
+            <p>my travel journal.</p>
+        </nav>
+    );
+}
+ 
+export default Navbar;
